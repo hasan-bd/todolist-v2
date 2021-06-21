0a1145ed8a8269799bedbbb911c82567432d76a4
hasan-bd <kamrulhasan570@gmail.com>
2021-06-21 12:18:17 +0600

    Insall mongoose package and Insert default items

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const date = require(__dirname + '/date.js')
+
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static ('public'))
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const itemsSchema = {
+  name: String
+}
+
+const Item = mongoose.model('item',itemsSchema)
+
+const item1 =new Item({
+  name: 'Eat'
+})
+
+const item2 = new Item ({
+  name: 'Sleep'
+})
+
+const item3 = new Item ({
+  name: 'Code'
+})
+
+const defaultItems = [item1,item2,item3]
+
+// Item.insertMany(defaultItems,function(err){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log('Successfully Save default items to Database');
+//   }
+// })
+
+app.get('/', (req, res) => {
+
+Item.find({},function(err,foundItems){
+  res.render('list', {listTitle: day, newListItems: foundItems})
+})
+
+  const day = date.getDate()
+
+})
+
+// Item.find({},function(err,items){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log(items);
+//   }
+// })
+
+app.post('/', function(req, res) {
+  const item = req.body.newItem
+  if(req.body.list==='work'){
+    workItem.push(item)
+    res.redirect('/work')
+  }else {
+    items.push(item)
+    res.redirect('/')
+  }
+
+})
+
+
+app.get('/work',function(req,res){
+  const day = date.getOnlyDay()
+  res.render('list',{listTitle: day +'s, Work', newListItems: workItem})
+})
+
+app.get('/about',function(req,res){
+  res.render('about')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
